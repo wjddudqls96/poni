75d5e99d4db24fe450bfb6600abe9fcbcd3be5ce
jybin96 <jybin96@changwon.ac.kr>
2024-06-22 04:24:17 +0900

    [feat] : 프린트 옵션 UI 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  assetsInclude: ['**/*.pdf'], // 이 줄을 추가하여 PDF 파일을 포함
+});
