d489d3ee02b64b048fcfb0a0b359d4c761b699a8
hyunwoo <wjdgusdndi@gmail.com>
2024-06-23 00:31:13 +0900

    [feat] : 스캔 페이지

--- frontend/src/components/dictation-page/Scan.tsx
+++ frontend/src/components/dictation-page/Scan.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Scan.css";
 
 const Scan: React.FC = () => {
   return (
     <div>
-      <div>
+      <div className="scan-text-box">
         <div>받아쓰기 완료</div>
         <div>작성한 답안지를 스캔해주세요</div>
       </div>
-      <div>출력 생성</div>
+      <div className="get-print-btn">출력 생성</div>
     </div>
   );
 };
