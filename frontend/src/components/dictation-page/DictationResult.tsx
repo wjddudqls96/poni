68ae94aacd324454d3e2fcfb045dbf8599c4af16
hyunwoo <wjdgusdndi@gmail.com>
2024-06-23 02:53:41 +0900

    [feat] : 결과 페이지 css

--- frontend/src/components/dictation-page/DictationResult.tsx
+++ frontend/src/components/dictation-page/DictationResult.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import "./DictationResult.css";
 
 const DictationResult: React.FC = () => {
   return (
     <div>
-      <div>~~~님</div>
-      <div>받아쓰기 결과는 ~~ 점입니다.</div>
-      <div>문제 ~개 정답 ~개 오답 ~개</div>
-      <div></div>
+      <div className="result-name-box">
+        <div>~~~님 </div>
+      </div>
+      <div className="result-text-box">
+        <div>받아쓰기 결과는</div>
+        <div className="result-score">~~점</div>
+        <div>입니다.</div>
+      </div>
+
+      <div>
+        <div>문제</div>
+        <div>~개</div>
+      </div>
+      <div>
+        <div>정답</div>
+        <div>~개</div>
+      </div>
+      <div>
+        <div>오답</div>
+        <div>~개</div>
+      </div>
       <div>
         <div>다시하기</div>
         <div>오답노트 생성</div>
