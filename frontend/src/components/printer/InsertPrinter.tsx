075fefdb030a53a5566386fda08061f2fe74a1ad
hyunwoo <wjdgusdndi@gmail.com>
2024-06-23 17:16:46 +0900

    [feat] : 기기 ID 입력

--- frontend/src/components/printer/InsertPrinter.tsx
+++ frontend/src/components/printer/InsertPrinter.tsx
@@ -1,4 +1,7 @@
 import React, { ChangeEvent, useState } from "react";
+import "./InsertPrinter.css";
+import { useNavigate } from "react-router-dom";
+import axios from "../../service/axiosConfig";
 
 const InsertPrinter: React.FC = () => {
   const [printerId, setPrinterId] = useState("");
@@ -7,15 +10,36 @@ const InsertPrinter: React.FC = () => {
     setPrinterId(event.target.value);
   };
 
+  const navigate = useNavigate();
+  const submitPrinterId = () => {
+    const response = axios.post("/update", {
+      deviceId: printerId,
+      language: "english",
+    });
+    navigate("/main");
+  };
+
   return (
     <div>
-      <div>복합기 아이디를</div>
-      <div>입력해주세요</div>
-      <div>
-        <div>복합기 아이디</div>
-        <input type="text" value={printerId} onChange={handleInputChange} />
+      <div className="insert-printer-container">
+        <div className="message-box">
+          <div>Please input your</div>
+          <div>Multifunction Device ID</div>
+        </div>
+        <div className="input-box-tag">Multifunction Device ID</div>
+        <div className="input-box">
+          <input
+            className="input-text-box"
+            type="text"
+            value={printerId}
+            onChange={handleInputChange}
+            placeholder="Enter device ID here"
+          />
+        </div>
       </div>
-      <div>완료</div>
+      <div className="submit-btn" onClick={submitPrinterId}>
+        Submit
+      </div>
     </div>
   );
 };
